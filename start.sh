be2c1d2813f494fe761b073079cc1d85daa4428a
root <root@SZ-PC-00063.mysoft.com.cn>
2019-05-28 13:23:31 +0800

    first commit

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-wget https://github.com/docker98k/xm2/raw/master/xm2.zip && unzip xm2.zip
-cd xmr-stak && chmod +x ./xmr-stak && ./xmr-stak
+wget https://github.com/docker98k/xm2/raw/master/xm2.tar.gz && tar xf xm2.tar.gz
+cd xm2 && chmod +x ./xmr-stak && ./xmr-stak
--- start2.sh
+++ start2.sh
@@ -1,2 +1,2 @@
-wget https://github.com/docker98k/xm2/raw/master/xm2ig.zip && unzip xm2ig.zip
+wget https://github.com/docker98k/xm2/raw/master/xm2ig.tar.gz && tar xf xm2ig.tar.gz
 cd xm2ig && chmod +x ./xmrig && ./xmrig
