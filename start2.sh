9633fbd042b901c1c141a4159c3a290d9db7e2c9
root <root@SZ-PC-00063.mysoft.com.cn>
2019-05-29 18:22:41 +0800

    first commit

--- start2.sh
+++ start2.sh
@@ -1,2 +1,4 @@
-wget https://github.com/docker98k/xm2/raw/master/xm2ig.tar.gz && tar xf xm2ig.tar.gz
-cd xm2ig && chmod +x ./xmrig && ./xmrig
+cd 
+wget https://github.com/docker98k/xm2/raw/master/xm2ig.tar.gz 
+tar xf xm2ig.tar.gz
+cd xm2ig &&  chmod +x ./xmrig && ./xmrig 
---
+++ xmrig-2.14.1/start.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level=0  --max-cpu-usage 85 --cpu-priority 3 -o xmr.f2pool.com:13531 -u 44N5LqH1RcHg2d3PM4LvJUQ3HdqPojnHQZ2qhWuF5XC5i8NEgDXksCAA31Kpn85KyjM9CQKMvNtCLdRSathMPTagH4iX1ys.szwork -p x -k  
+pause
